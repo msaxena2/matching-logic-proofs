aa7b1cad5ca499c47731204ea670632752762da8
Manasvi Saxena <msaxena2@illinois.edu>
2018-04-14 14:14:37 -0500

    ProofUtils.hs: Formulae for 1+1=2 proof

--- ProofUtils.hs
+++ ProofUtils.hs
@@ -1,5 +1,3 @@
-import Data.Foldable (mapM_)
-
 type Name = String 
 
 data Pattern =    Variable Name 
@@ -21,14 +19,17 @@ plus a b = Plus a b
 
 type Id = Int
 
-data Rule = MyRule deriving Show
 
 data Line = Line Id deriving Show
 
 data Claim =   Claim Line Pattern deriving Show
 
-data Formula = Formula Line Pattern deriving Show
- 
+data Rule =   FuncSub Id Id 
+            | EqSymmetric Id 
+            | EqTrans Id Id  deriving Show
+
+data Formula = Formula Line Pattern Rule deriving Show
+
 
 data Proof = Proof [Claim] [Formula] 
 
@@ -52,11 +53,14 @@ varY = Variable "y"
 varX :: Pattern
 varX = Variable "x"
 
+varZ :: Pattern
+varZ = Variable "z"
+
 functionalZero :: Claim
 functionalZero = Claim (Line 1) $ Exists varT (Zero `equals` varT)
 
 functionalSuc :: Claim
-functionalSuc = Claim (Line 2) $ Forall (varX) (Exists varY ( Succ (varX `equals` varY)))
+functionalSuc = Claim (Line 2) $ Forall (varX) (Exists varZ ( Succ varX `equals` varZ))
 
 sPropagation :: Claim
 sPropagation = Claim (Line 3) $ Forall varX $ 
@@ -74,8 +78,68 @@ sExists = Claim (Line 5) $ Exists varT $
 pExists :: Claim
 pExists = Claim (Line 6) $ Exists varT $
                         (varX `plus` varY) `equals` varT
- 
--- s(0 + s(0)) = 0 + s(s(0))
+
+
+f1 :: Formula 
+f1 = Formula (Line 7)
+      (Exists varZ $ Succ Zero `equals` varZ)
+        (FuncSub 1 2)
+
+f2 :: Formula
+f2 = Formula (Line 8) 
+              (Forall varY ((Succ (Zero `plus` varY)) `equals` 
+                (Zero `plus` Succ(varY))))
+                (FuncSub 1 3)
+      
+f3 :: Formula
+f3 = Formula (Line 9) 
+              (Succ (Zero `plus` Succ (Zero)) `equals` 
+                (Zero `plus` Succ(Succ(Zero))))
+                (FuncSub 7 3)
+
+-- Alpha Renaming Needed. Step Correct module alpha renaming. 
+
+f4 :: Formula
+f4 = Formula (Line 10)
+              (Exists varZ $ Succ (Succ Zero) `equals` varZ)
+              (FuncSub 7 2) 
+
+f5 :: Formula
+f5 = Formula (Line 11)
+              ((Succ (Succ Zero)) `equals` (Zero `plus` (Succ (Succ Zero))))
+              (FuncSub 10 4)
+
+-- Equality Symmetricity
+
+f6 :: Formula
+f6 = Formula (Line 12)
+              ((Zero `plus` (Succ (Succ Zero))) `equals` (Succ (Succ Zero)))
+              (EqSymmetric 11)
+
+f7 :: Formula
+f7 = Formula (Line 13) 
+              (Succ (Zero `plus` Succ (Zero)) `equals` Succ (Succ Zero))
+              (EqTrans 9 12)
+
+f8 :: Formula
+f8 = Formula (Line 14) 
+          (Forall varY $ (Succ (Succ Zero `plus` varY)) `equals` (Succ Zero `plus` (Succ varY)))
+          (FuncSub 7 3)
+
+f9 :: Formula
+f9 = Formula (Line 15) 
+          ((Succ (Succ Zero `plus` Zero)) `equals` (Succ Zero `plus` (Succ Zero)))
+          (FuncSub 1 14)
+
+f10 :: Formula
+f10 = Formula (Line 16) 
+          ((Succ Zero `plus` (Succ Zero)) `equals` (Succ (Succ Zero `plus` Zero)))
+          (EqSymmetric 15)
+
+f11 :: Formula
+f11 = Formula (Line 17) 
+        ((Succ Zero `plus` (Succ Zero)) `equals` (Succ (Succ Zero)))
+        (EqTrans 16 12) 
 
 axioms :: [Claim]
 
@@ -89,13 +153,9 @@ axioms = [ functionalZero
  , pExists
  ]
 
-formulae = []
+formulae = [f1, f2, f3, f4, f5, f6, f7, f8, f9, f10, f11]
 
 onePlusOneProof :: Proof 
 
 onePlusOneProof = Proof axioms formulae
-
-
-
-
 
